d60a737b8c5bd8888b3bf3c5a5238cc84faf8b74
Ivan Konoval <keks.ivan.k@gmail.com>
2024-06-21 11:26:59 +0200

    added_logic_for_logining

--- apps/auth/routes.py
+++ apps/auth/routes.py
@@ -1,19 +1,37 @@
 from flask import redirect, url_for, Blueprint, request, flash, render_template
-from flask_security import logout_user
+from flask_security import logout_user, login_user
 from marshmallow import ValidationError
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from apps import user_datastore
-from apps.auth.forms import RegisterForm
+from apps.auth.forms import RegisterForm, LoginForm
+from apps.auth.models import User
 from apps.database import db
-from apps.auth.shemas import RegisterSchema
+from apps.auth.shemas import RegisterSchema, LoginSchema
 
 module = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @module.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        login_schema = LoginSchema()
+        try:
+            serialized_data = login_schema.load({
+                "email": form.email.data,
+                "password": form.password.data
+            })
+            user = User.query.filter_by(email=serialized_data['email']).first()
+            if user and check_password_hash(user.password, serialized_data["password"]) and user.active:
+                login_user(user)
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for('home.home'))
+            else:
+                flash('Invalid username or password.', 'error')
+        except ValidationError as ex:
+            flash(ex.messages, 'error')
+    return render_template('auth/login.html', form=form)
 
 
 @module.route('/register', methods=['GET', 'POST'])
@@ -35,7 +53,7 @@ def register():
             db.session.commit()
             return redirect(url_for('home.home'))
         except ValidationError as ex:
-            flash(f'Validation error: {ex.messages}', 'error')
+            flash(ex.messages, 'error')
     return render_template('auth/register.html', form=form)
 
 
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from dotenv import load_dotenv
 
@@ -12,5 +13,10 @@ class AppConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SESSION_PERMANENT = os.environ['SESSION_PERMANENT']
+    SECURITY_PASSWORD_HASH = os.environ['SECURITY_PASSWORD_HASH']
+    PERMANENT_SESSION_LIFETIME = int(os.environ['PERMANENT_SESSION_LIFETIME'])
+    REMEMBER_COOKIE_DURATION = timedelta(days=int(os.environ['REMEMBER_COOKIE_DURATION_DAYS']))
 
 # TODO: in future add ProdAppConfig(AppConfig) and LocalAppConfig(AppConfig)
