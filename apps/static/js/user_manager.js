1e2e84e1327a9b0042b3f0fbdaef09afcdb7c58c
IvanK <knbgjkkmk@gmail.com>
2024-07-27 22:11:34 +0200

    first_commit_before_sleep

---
+++ apps/static/js/user_manager.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let addButtons = document.querySelectorAll('.btn__add');
+    let subButtons = document.querySelectorAll('.btn__sub');
+    let selectElements = document.querySelectorAll('.role__element');
+
+    addButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            let userId = this.getAttribute('data-user-id');
+            let selectElement = Array.from(selectElements).find(select => select.getAttribute('data-user-id') === userId);
+
+            if (selectElement) {
+                let selectedRole = selectElement.value;
+                let href = this.getAttribute('href');
+                let finalUrl = href.replace('ROLE_NAME', encodeURIComponent(selectedRole));
+
+                this.setAttribute('href', finalUrl);
+            }
+        });
+    });
+    subButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            let userId = this.getAttribute('data-user-id');
+            let selectElement = Array.from(selectElements).find(select => select.getAttribute('data-user-id') === userId);
+
+            if (selectElement) {
+                let selectedRole = selectElement.value;
+                let href = this.getAttribute('href');
+                let finalUrl = href.replace('ROLE_NAME', encodeURIComponent(selectedRole));
+
+                this.setAttribute('href', finalUrl);
+            }
+        });
+    });
+});
